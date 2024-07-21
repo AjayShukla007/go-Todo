0d48e1cea44b945cf45500c43954ee99e0c49428
AjayShukla007 <shuklaajay0838@gmail.com>
2024-07-21 10:10:21 +0530

    Add get api route

--- main.go
+++ main.go
@@ -58,8 +58,9 @@ func main() {
 }
 
 func getHandler(c *fiber.Ctx) error {
-	
+
 	var todos []Todo
+	
 	cursor, err := collection.Find(context.Background(), bson.M{})
 	if err != nil {
 		return err
