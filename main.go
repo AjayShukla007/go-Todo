8ae2233feeb367933e116b4e94cba7da84ee4e01
AjayShukla007 <shuklaajay0838@gmail.com>
2024-08-03 10:10:21 +0530

    Add updatr api route

--- main.go
+++ main.go
@@ -92,8 +92,9 @@ func postHandler(c *fiber.Ctx) error {
 }
 
 func updateHandler(c *fiber.Ctx) error {
-	
+
 	id := c.Params("id")
+	
 	objectId, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
 		return c.Status(400).JSON(fiber.Map{"error": "empty id"})
